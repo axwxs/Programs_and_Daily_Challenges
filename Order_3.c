5a092a8aafd3769fdc8a04575498c5a3e652724a
Ali R <72666513+axwxs@users.noreply.github.com>
2021-06-14 20:36:36 -0400

    Update Order_3.c

--- Order_3.c
+++ Order_3.c
@@ -1,4 +1,5 @@
 /*
+
 Required to write a function, called orderThree which rearranges three numbers in 
 increasing order. The function should return nothing (void as the return type) and 
 take three parameters. After calling the function in the main function, the double 
@@ -12,6 +13,7 @@ If the inputs are "23.558 12 -24.2" the output should be "-24.20 12.00 23.55"
 Some constraints:
 In the main function you can only call the function you wrote. Nothing else should be
 added or altered. Can't use any global variables in the program.
+
 */
 
 #include <stdio.h>
@@ -22,14 +24,14 @@ void orderThree(double *a, double *b, double *c);
 int main(int argc, char * argv[]) {
 	double num1, num2, num3;
     
-    printf("Enter three numbers separated by blanks:\n");
-    scanf("%lf%lf%lf", &num1, &num2, &num3);
+	printf("Enter three numbers separated by blanks:\n");
+	scanf("%lf%lf%lf", &num1, &num2, &num3);
     
-    // Function call to order the 3 numbers in increasing order
-    orderThree(&num1, &num2, &num3);
+	// Function call to order the 3 numbers in increasing order
+	orderThree(&num1, &num2, &num3);
     
     
-    // Display the Results
+	// Display the Results
 	printf("The numbers in ascending order are: %.2f %.2f %.2f\n", num1, num2, num3);
     
 	return 0;
