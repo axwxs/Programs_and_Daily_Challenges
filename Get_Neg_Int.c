c1080a0e26769684f9246c040772a4561682a905
Ali R <72666513+axwxs@users.noreply.github.com>
2021-06-15 22:31:07 -0400

    Create Get_Neg_Int.c

---
+++ Get_Neg_Int.c
@@ -0,0 +1,46 @@
+/*
+
+Author: <Ali Rashid>
+Date: <02/16/2021>
+Purpose: <Output negative number from input>
+
+*/
+
+#include <stdio.h>
+
+void clearKeyboardBuffer(void);
+
+
+int getNegInt() {
+    int varInt = 1;
+    printf("Please enter a negative number:\n");
+    
+    while(varInt >= 0) {
+    scanf("%d", &varInt);
+    clearKeyboardBuffer();
+        if(varInt >=0) {
+            printf("I'm sorry, you must enter a negative integer less than zero:\n");
+        }
+    }
+
+    printf("I got the number %d!\n", varInt);
+    return varInt;
+   
+}
+
+int main(int argc, char *argv[]) {
+    getNegInt();
+    
+    // int varInt = getNegInt();
+    // printf("%d\n", varInt);
+
+    return 0;   
+}
+
+void clearKeyboardBuffer(void) {
+    char ch;
+    scanf("%c", &ch);
+    while (ch != '\n' && ch != '\0') {
+	      scanf("%c", &ch);
+    }
+}
