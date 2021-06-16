51d99aae41b58ac5041db984499288a72d7b59c8
Ali R <72666513+axwxs@users.noreply.github.com>
2021-06-15 22:06:06 -0400

    Update Find_Identical_Pair.c

--- Find_Identical_Pair.c
+++ Find_Identical_Pair.c
@@ -21,30 +21,30 @@ out of bounds accessing.
 
 
 int main(int argc, char * argv[]) {
-   double arr[10] = { 1.2, 6.1, 3.3, 3.3, 4.5, 7.9, 5.4, 5.4, 9.9, 1.0};
-   
-   // Another test array for debugging below
-   // double arr2[6] = {1.2, 6.1, 3.3, 3.3, 4.5};
+    double arr[10] = { 1.2, 6.1, 3.3, 3.3, 4.5, 7.9, 5.4, 5.4, 9.9, 1.0};
     
-   int index = 0;
-   
-   // for some reason sizeof function doesn't work?
-   // int length = sizeof(arr)/sizeof(arr[0]);
-   
-   // the below line only works in the main function for some reason
-   // int length = *(&arr + 1) - arr;
-   
-   // all the function to get the index
+    // the array below is for testing/debugging
+    // double arr2[6] = {1.2, 6.1, 3.3, 3.3, 4.5};
     
-   index = findIdenticalPair(arr, 10);
+    int index = 0;
     
+    // for some reason sizeof function doesn't work?
+    // int length = sizeof(arr)/sizeof(arr[0]);
+    
+    // the below line only works in the main function for some reason
+    // int length = *(&arr + 1) - arr;
+    
+    // all the function to get the index
+    
+    index = findIdenticalPair(arr, 10);
+    
+    
+    printf("index is: %d\n", index);
+    
+    // below line is for debugging the second test array
+    // printf("index is: %d\n", findIdenticalPair(arr2, 5));
    
-   printf("index is: %d\n", index);
-   
-   // Below line is for debugging second test array
-   // printf("index is: %d\n", findIdenticalPair(arr2, 5));
-   
-   return 0;
+    return 0;
 }
 
 /*
@@ -56,17 +56,17 @@ It avoids illegal out of bounds accessing by having an integer passed into the f
 which then serves as a limit for incrementation during the comparison process.
 */
 int findIdenticalPair(double arr[], int a) {
-   int i = 0;
-   int indexValue;
+    int i = 0;
+    int indexValue;
    
-   while (i < a) {
+    while (i < a) {
       if (arr[i] == arr[i+1]) {
          indexValue = i;
          return indexValue;
       }
       i++;
-   }
-   
-   // If there's no other return value this is the default.
-   return -1;   
+    }
+    
+    // if there's no other return value this is the default.
+    return -1;   
 }
